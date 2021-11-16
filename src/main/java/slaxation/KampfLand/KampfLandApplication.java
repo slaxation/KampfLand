bad03a45b24ed371bdbb181684ca164c027940af
Peter Bornstein <zmaster.allstar@gmail.com>
2021-11-16 17:50:32 +0100

    Initial commit

---
+++ src/main/java/slaxation/KampfLand/KampfLandApplication.java
@@ -0,0 +1,16 @@
+package slaxation.KampfLand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KampfLandApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(KampfLandApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/slaxation/KampfLand/KampfLandApplicationTests.java
@@ -0,0 +1,13 @@
+package slaxation.KampfLand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KampfLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
